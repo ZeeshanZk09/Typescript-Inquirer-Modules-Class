5127c6b79c6606fe943d0c1aca5cb60b1797e231
Entreprenuer Zeeshan <dr5269139@gmail.com>
2024-05-01 15:45:55 +0500

    Typescript-Inqurirer-Modules-Class

---
+++ main.ts
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([{
+    name : "age",
+    type : "number",
+    message : "Enter you age:",
+}]);
+
+console.log(`Inshallah ${60-answers.age} you will be 60 years old.`);
